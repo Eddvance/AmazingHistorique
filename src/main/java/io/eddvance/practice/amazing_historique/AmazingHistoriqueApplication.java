6f40aa86bfb0a55ecb313e732733b96b2cc195d5
Edouard Leroy <leroyedouard@protonmail.com>
2025-03-08 12:41:38 +0100

    Start

---
+++ src/main/java/io/eddvance/practice/amazing_historique/AmazingHistoriqueApplication.java
@@ -0,0 +1,13 @@
+package io.eddvance.practice.amazing_historique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingHistoriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingHistoriqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/eddvance/practice/amazing_historique/AmazingHistoriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddvance.practice.amazing_historique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingHistoriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
