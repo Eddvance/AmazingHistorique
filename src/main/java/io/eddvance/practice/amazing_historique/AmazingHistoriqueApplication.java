a445b3285e1d1aa8b593f2db231d09b1553815bf
Edouard Leroy <leroyedouard@protonmail.com>
2025-03-24 17:23:16 +0100

    ms translator histo in progress 4.1

--- src/main/java/io/eddvance/practice/amazing_historique/AmazingHistoriqueApplication.java
+++ src/main/java/io/eddvance/practice/amazing_historique/AmazingHistoriqueApplication.java
@@ -3,6 +3,7 @@ package io.eddvance.practice.amazing_historique;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AmazingHistoriqueApplication {
     public static void main(String[] args) {
